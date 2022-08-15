5b6ab2050e92150ad1a3547b8f7ae742d135465b
Cooper Viktor <cooper@gracepapers.com.au>
2022-08-15 12:56:47 +1000

    Adding base button

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PrimaryButton = styled.button`
+  background-color: blue;
+  color: white;
+  font-size: 1rem;
+  border: none;
+`;
+
+export default PrimaryButton;
