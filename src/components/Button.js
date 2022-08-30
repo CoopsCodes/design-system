27ebd01f04a4bb59447f533ca587c244b2e22b0c
Cooper Viktor <cooper@gracepapers.com.au>
2022-08-30 18:38:56 +1000

    main styled button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,45 @@
 import styled from "styled-components";
 
-const PrimaryButton = styled.button`
-  background-color: blue;
-  color: white;
+const Button = styled.button`
   font-size: 1rem;
-  border: none;
+  border-style: none;
+  padding: 0;
+  background-color: #00a3ff;
+  cursor: pointer;
+  user-select: none;
+  transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
 `;
 
-export default PrimaryButton;
+const Span = styled.span`
+  border: solid;
+  border-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  backdrop-filter: blur(2px);
+  width: 220px;
+  height: 50px;
+  padding: 0;
+  transform: translate(0px, 0px);
+  transition: transform 600ms cubic-bezier(0.3, 0.7, 0.4, 1);
+  &:focus-visible {
+    outline: revert;
+  }
+  &:hover,
+  &:focus-visible {
+    transform: translate(6px, -6px);
+    transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+  }
+  &:active {
+    transform: translate(0px, 0px);
+    transition: transform 50ms;
+  }
+`;
+
+export default function PrimaryButton({ children, ...props }) {
+  return (
+    <Button {...props}>
+      <Span {...props}>{children}</Span>
+    </Button>
+  );
+}
